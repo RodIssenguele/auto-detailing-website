b98cc8d1f44b223416292a8727d961df37b54458
rodrigo <rodrigoissenguele@gmail.com>
2026-08-21 07:50:55 +0200

    correction of Html semantics and syntax. mobile-header, mobile-nav and mobile-hero, 90% finished for now, but still open for UX desing improvements. dynamic mobile nav menu created 3strips when closed and x when opened

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function navBtn(){
   const navMobileBtn = document.querySelector('.js-nav-mobile-btn');
+  const navContainer = document.querySelector('.js-nav-container');
+  const menuIcon = document.querySelector(".hamburger-icon");
+
 
   navMobileBtn.addEventListener('click', ()=>{
 
-    const navContainer = document.querySelector('.js-nav-container');
+    navContainer.classList.toggle('nav-container-visible');
 
-    navContainer.classList.toggle('nav-container-visible')
+    const menuIsOpen = navContainer.classList.contains('nav-container-visible');
 
+    
+    navMobileBtn .setAttribute("aria-expanded", menuIsOpen);
+  
+    if(menuIsOpen){
+      menuIcon.innerHTML = 'X'  
+    } else{
+      menuIcon.innerHTML = '☰'
+    }
+    console.log(navMobileBtn.attributes)
   })
 }
 
